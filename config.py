c58ef0fdbe62734b6206925efd08f76382bee3fc
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-17 13:14:44 +0100

    Migrate to Pipenv

---
+++ config.py
@@ -0,0 +1,21 @@
+EDITIONS = {
+    "2015": {
+        "EVENT_DATETIME": "19/11/2016T21:00",
+        "DAYS_TO_SIGNUP": 10,
+        "EMBEDMAP_SRC": "https://www.google.com/maps/embed?\
+pb=!1m18!1m12!1m3!1d3506.6425042707415!2d-16.32172299999999\
+5!3d28.490310999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f1\
+3.1!3m3!1m2!1s0xc41cc578c377601%3A0x77541f49aeeb2ba6!2sRest\
+aurante+El+Esquinazo!5e0!3m2!1ses!2ses!4v1524158792823",
+    },
+    "2016": {
+        "EVENT_DATETIME": "19/11/2016T21:00",
+        "DAYS_TO_SIGNUP": 10,
+        "EMBEDMAP_SRC": "https://www.google.com/maps/embed?\
+pb=!1m18!1m12!1m3!1d876.6589320132223!2d-16.323250170802883\
+!3d28.49051499890464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.\
+1!3m3!1m2!1s0xc41cdc0281494ef%3A0x49044255fdfe2584!2sStefan\
+os+la+Laguna!5e0!3m2!1ses!2ses!4v1477222434193"
+    }
+}
+DATAFILE = "data/assistants_{}.csv"
--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,17 @@
-from fabric.api import env, local, prefix, cd, run, get
+from fabric.api import env, local, cd, run, get
 import datetime
 
-env.hosts = ["production"]
+env.hosts = ['production']
 
 
 def deploy():
-    local("git push")
-    with prefix("source ~/.virtualenvs/csi/bin/activate"):
-        with cd("~/csi"):
-            run("git pull")
-            run("pip install -r requirements.txt")
-            run("bower install")
-            run("supctl restart csi")
+    local('git push')
+    with cd('~/csi'):
+        run('git pull')
+        run('pipenv install')
+        run('bower install')
+        run('supctl restart csi')
 
 
 def get_assistants(year=datetime.datetime.today().year):
-    get("~/csi/data/assistants_{}.csv".format(year), "data/")
+    get('~/csi/data/assistants_{}.csv'.format(year), 'data/')
